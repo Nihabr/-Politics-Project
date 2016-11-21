a6ddf967cb1b54d511bf5a914579fa3a254a8473
Niklas Broge <nihabr@gmail.com>
2016-11-21 13:21:32 +0100

    Added test version of scraper. Test purpose: Checking whether the current directory will be accepted. Testing whether new code will reduce load on server RAM.

---
+++ src/Test/ScrapingRobotTest.py
@@ -0,0 +1,54 @@
+'''
+Created on Sep 21, 2016
+
+@author: nihab
+'''
+import praw
+import csv
+import unicodedata
+import datetime as dt
+
+from Test import secrets
+
+csvfilepath = 'PoliticsLim0.csv'
+
+
+
+def run_bot(r):
+    counter = 0
+    subcounter = 0
+    subreddit = r.get_subreddit("politics")
+    submissions = subreddit.get_hot(limit = 5)
+    columns = ['counter', 'comment body', 'date', 'karma']
+        
+    
+
+        
+    with open(csvfilepath,'w', newline='', encoding='utf8') as mf:
+        wr = csv.writer(mf, delimiter=',')
+        wr.writerow(columns)
+        for submission in submissions:
+            comments = submission.comments
+            subcounter += 1
+            print (subcounter)
+            for comment in comments: 
+                if (hasattr(comment,'body')):
+                           
+                    counter += 1
+                    body = comment.body
+                    karma = comment.score
+                    time = comment.created
+                    time = dt.datetime.fromtimestamp(time)
+                    c = [counter, body, time, karma]
+                    wr.writerow(c)
+            
+        mf.close()                
+      
+def login():
+    r = praw.Reddit(user_agent = secrets.APP_USER_AGENT)
+    r.set_oauth_app_info(secrets.APP_ID, secrets.APP_SECRET, secrets.APP_URI)
+    r.refresh_access_information(secrets.APP_REFRESH_TOKEN)
+    return r    
+
+r = login()
+run_bot(r)    
