ec14b0649ae80ba1e7f9b8211d79a5f08a997390
Niklas Broge <nihabr@gmail.com>
2016-11-21 14:00:05 +0100

    ScrapingRobot update 21-11-16 14:00

--- src/Test/ScrapingRobot.py
+++ src/Test/ScrapingRobot.py
@@ -5,12 +5,11 @@ Created on Sep 21, 2016
 '''
 import praw
 import csv
-import unicodedata
 import time as tm
 import datetime as dt
 import secrets
 
-csvfilepath = 'PoliticsLim0.csv'
+csvfilepath = 'DatasetV1.csv'
 
 '''praw.helpers.submissions_between(reddit_session,
              subreddit, lowest_timestamp=None, highest_timestamp=None, newest_first=True, 
@@ -28,27 +27,29 @@ def run_bot(r):
     '''submissions = subreddit.get_hot(limit = 0)'''
     submissions = praw.helpers.submissions_between(r, subreddit, lowest_timestamp=lts, highest_timestamp=hts, newest_first=True)
     columns = ['counter', 'comment body', 'date', 'karma']
-    commentlist = [columns]      
+        
     
-    for submission in submissions:
-        comments = submission.comments
-        subcounter += 1
-        print (subcounter)
-        for comment in comments: 
-            if (hasattr(comment,'body')):
-                       
-                counter += 1
-                body = comment.body
-                karma = comment.score
-                time = comment.created
-                time = dt.datetime.fromtimestamp(time)
-                c = [counter, body, time, karma]
-                commentlist.append(c)
+
         
     with open(csvfilepath,'w', newline='', encoding='utf8') as mf:
-            wr = csv.writer(mf, delimiter=',')
-            wr.writerows(commentlist)
-            mf.close()                
+        wr = csv.writer(mf, delimiter=',')
+        wr.writerow(columns)
+        for submission in submissions:
+            comments = submission.comments
+            subcounter += 1
+            print (subcounter)
+            for comment in comments: 
+                if (hasattr(comment,'body')):
+                           
+                    counter += 1
+                    body = comment.body
+                    karma = comment.score
+                    time = comment.created
+                    time = dt.datetime.fromtimestamp(time)
+                    c = [counter, body, time, karma]
+                    wr.writerow(c)
+            
+        mf.close()                
       
 def login():
     r = praw.Reddit(user_agent = secrets.APP_USER_AGENT)
